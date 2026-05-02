425c391803e9aa7a79f5e81a97685da7a737e59e
Cacapon Project <cacapon.project@gmail.com>
2026-05-02 21:03:16 +0900

    リリース用スクリプトを作成

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+  echo "Usage: npm run release <version>"
+  echo "Example: npm run release 0.4.0"
+  exit 1
+fi
+
+jq --arg v "$VERSION" '.version = $v' manifest.json > tmp.json && mv tmp.json manifest.json
+git add manifest.json
+git commit -m "chore: bump version to $VERSION"
+git push
+git tag "$VERSION"
+git push origin "$VERSION"
